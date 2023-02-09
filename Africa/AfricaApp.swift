01e7b40439822486ed2c30c85b3a1e98d4d4ba27
Zach Johnson <zgjohnson94@gmail.com>
2023-02-08 19:22:39 -0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Zach Johnson on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
