a81ee24bffe26db96698cf6c9322f3809ecea0ff
Zach Johnson <zgjohnson94@gmail.com>
2023-02-12 17:28:40 -0600

    Lectures 6 and 7

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let haptictImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        Text("Video")
+        NavigationView {
+            List {
+                ForEach(videos) { video in
+                    NavigationLink(destination: VideoPlayerView(videoSelected: video.id, videoTitle: video.name))
+                    {
+                        VideoListItemView(video: video)
+                            .padding(.vertical, 8)
+                    }
+                }//: LOOP
+            }//: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                        haptictImpact.impactOccurred()
+                    }) {
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        }//: NAVIGATION
     }
 }
 
--- Africa/Model/VideoModel.swift
+++ Africa/Model/VideoModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    
+    // Computed Property
+    var thumbnail: String {
+        "video-\(id)"
+    }
+}
